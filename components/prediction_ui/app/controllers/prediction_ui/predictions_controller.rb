eb09ba5b72baa5d4703465ec348983468fead7e3
Alexander Khlipun <alex.khlipun@icloud.com>
2018-12-08 22:44:48 +0200

    Complete chapter 6.2: Data-Context-Interaction (DCI)

--- components/prediction_ui/app/controllers/prediction_ui/predictions_controller.rb
+++ components/prediction_ui/app/controllers/prediction_ui/predictions_controller.rb
@@ -1,5 +1,3 @@
-require_dependency "prediction_ui/application_controller"
-
 module PredictionUi
   class PredictionsController < ApplicationController
     def new
@@ -7,11 +5,45 @@ module PredictionUi
     end
     
     def create
-      predictor = ::Predictor::Predictor.new(Teams::Team.all)
-      predictor.learn(Games::Game.all)
-      @prediction = predictor.predict(
+      @prediction = GamePredictionContext.call(
+        Teams::Team.all,
+        Games::Game.all,
         Teams::Team.find(params["first_team"]["id"]),
         Teams::Team.find(params["second_team"]["id"]))
     end
+  end
+end
+
+class GamePredictionContext
+  def self.call(teams, games, first_team, second_team)
+    GamePredictionContext.new(
+      teams, games, first_team, second_team).call
+  end
+  
+  def initialize(teams, games, first_team, second_team)
+    @contenders = teams
+    @contenders.each do |contender|
+      contender.extend Predictor::Role::Contender
+    end
+    
+    @hpis = games
+    @hpis.each do |game|
+      game.extend Predictor::Role::HistoricalPerformanceIndicator
+    end
+    
+    @predictor = first_team
+    @predictor.extend Predictor::Role::Contender
+    @predictor.extend Predictor::Role::Predictor
+    
+    @second_team = second_team
+    @second_team.extend Predictor::Role::Contender
+  end
+  
+  def call
+    @predictor.opponent = @second_team
+    @predictor.contenders = @contenders
+    
+    @predictor.learn(@hpis)
+    @predictor.predict
   end
 end
--- components/predictor/lib/predictor.rb
+++ components/predictor/lib/predictor.rb
@@ -1,6 +1,9 @@
 require "saulabs/trueskill"
 
 module Predictor
-  require "predictor/predictor"
+  require "predictor/role/contender"
+  require "predictor/role/predictor"
+  require "predictor/role/historical_performance_indicator"
   require "predictor/prediction"
+  require "predictor/prediction_error"
 end
